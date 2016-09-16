8f65bfed7b17cbf1926d7854f4a13edacddcb0dc
Andrey Balanuk <abalanuk@lohika.com>
2016-09-16 21:18:49 +0300

    Http server

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-//console.log("Hello dude!");
-document.body.style.background = "green";
+//document.body.style.background = "green";
+
 var http = require('http');
+var heapdump = require('heapdump');
 
-http.createServer(function(request,response){
-	console.log(request.url);
-	response.writeHead(200);
-	response.write('<h2>Hello, this is the Http server<h2>');
-	response.end();
-}).listen(8080, function(){ console.log("Listening on port 8080..."); });
+var server = http.createServer(function(req,res){
+	if(req.url === '/') {
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write('Hello, this is the <h2>Http server<h2>');
+		res.end();
+	} else if(req.url === '/test'){
+		if(req.method === 'POST') {
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.write('Posted test message');
+			res.end();
+		}
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.end('Got: <h2>Http server<h2>');
+	}
 
+});
+
+server.listen(8080);
