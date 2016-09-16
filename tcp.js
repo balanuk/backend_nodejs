5a14bd1fae1aa5657060d2d3bc663f4b04a77e9a
Andrey Balanuk <abalanuk@lohika.com>
2016-09-16 21:23:09 +0300

    Adding and starting tcp server

--- tcp.js
+++ tcp.js
@@ -1,3 +1,15 @@
-/**
- * Created by abalanuk on 9/16/16.
- */
+var net = require('net');
+
+var server = net.createServer(function(client){
+	console.log('client connected');
+	client.on('data', function(data){
+		console.log('data received: ' + data.toString());
+	});
+	client.on('end', function(){
+		console.log('client disconnected')
+	});
+});
+
+server.listen(3000, function(){
+	console.log('server started');
+});
