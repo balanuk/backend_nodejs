c9112e44f9397a7d84aa1607743abb00723dc49b
Andrey Balanuk <abalanuk@lohika.com>
2016-09-20 16:39:06 +0300

    Added jquery,browserify deps as front-end part of infra-re plus test html/script

---
+++ app.js
@@ -0,0 +1,21 @@
+//document.body.style.background = "green";
+
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+	if(req.url === '/') {
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write('Hello, this is the <h2>Http server<h2>');
+		res.end();
+	} else if(req.url === '/test'){
+		if(req.method === 'POST') {
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.write('Posted test message');
+			res.end();
+		}
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.end('Got: <h2>Http server<h2>');
+	}
+});
+
+server.listen(8080);
--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-//document.body.style.background = "green";
+//example file of front-end part to familiarize with browserify tool
+var $ = require('jquery');
 
-var http = require('http');
-var heapdump = require('heapdump');
-
-var server = http.createServer(function(req,res){
-	if(req.url === '/') {
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.write('Hello, this is the <h2>Http server<h2>');
-		res.end();
-	} else if(req.url === '/test'){
-		if(req.method === 'POST') {
-			res.writeHead(200, {"Content-Type": "text/html"});
-			res.write('Posted test message');
-			res.end();
-		}
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.end('Got: <h2>Http server<h2>');
-	}
-
+$(document).ready(function(){
+	var button = $('<button/>').html('Click me dude').on('click', function(){ alert('Hey dude, you are clicked me :)!') });
+	$('body').append(button);
 });
-
-server.listen(8080);
